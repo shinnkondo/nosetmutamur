136fa1edda9170063924784a2ecd11e2cb33daf7
Shinnosuke Kondo <shinn.kondo@gmail.com>
2015-08-01 22:04:49 -0500

    Add image popup.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -3,4 +3,8 @@
 //= require "_include/ui-bootstrap-custom-0.13.0"
 //= require "underscore/underscore-min"
 //= require "_include/pourover"
+//= require "magnific-popup/dist/jquery.magnific-popup"
 
+$(document).ready(function() {
+  $('.img-popup-link').magnificPopup({type:'image', closeOnContentClick: true});
+});
