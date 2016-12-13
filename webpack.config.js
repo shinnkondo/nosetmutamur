dff8b8b274459eb8f138234c3fb12efc94813860
Shinn Kondo <shinn.kondo@gmail.com>
2016-12-13 16:25:15 -0600

    Improve to separate JS by path.

--- webpack.config.js
+++ webpack.config.js
@@ -3,50 +3,55 @@ const webpack = require('webpack');
 const path = require('path');
 
 const { CheckerPlugin } = require('awesome-typescript-loader')
-
+const SplitByPathPlugin = require('webpack-split-by-path');
 module.exports = [{
-        resolve: {
-            extensions: ['.ts', '.tsx', '.js', '.jsx']
-        },
-        entry: {
-            main: './source/javascripts/index.ts',
-            vendor: './source/javascripts/lib/index.ts'
-        },
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: '[name].js',
-        },
-        module: {
-            rules: [
-                { test: /\.ts$/, loader: "awesome-typescript-loader" },
-                { test: require.resolve('angular') , loader: 'exports-loader?window.angular'},
-                {
-                    test: /\.scss$/,
-                    loaders: ["style-loader", "css-loader", "sass-loader"]
-                },
-                {
-                    test: /\.css$/,
-                    loaders: ["style-loader", "css-loader"]
-                },
-                {
-                    test: /\.html$/,
-                    loader: ['ngtemplate-loader?requireAngular', 'html-loader']
-                }
-            ]
-        },
-        plugins: [
-            new webpack.ProvidePlugin({
-                $: "jquery",
-                jQuery: "jquery",
-                angular: "angular",
-                _: "underscore"
-            }),
-            new CheckerPlugin(),
-            new webpack.optimize.CommonsChunkPlugin({
-                names: ['vendor', 'manifest'] // Specify the common bundle's name.
-            })
-        ],
-        devtool: "cheap-eval-source-map"
-    }
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx']
+    },
+    entry: {
+        main: './source/javascripts/index.ts'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        chunkFilename: '[name].js',
+    },
+    module: {
+        rules: [
+            { test: /\.ts$/, loader: "awesome-typescript-loader" },
+            { test: require.resolve('angular'), loader: 'exports-loader?window.angular' },
+            {
+                test: /\.scss$/,
+                loaders: ["style-loader", "css-loader", "sass-loader"]
+            },
+            {
+                test: /\.css$/,
+                loaders: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.html$/,
+                loader: ['ngtemplate-loader?requireAngular', 'html-loader']
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            angular: "angular",
+            _: "underscore"
+        }),
+        new CheckerPlugin(),
+        new SplitByPathPlugin([
+            {
+                name: 'vendor',
+                path: path.join(__dirname, 'node_modules')
+            }
+        ], {
+                manifest: 'app-entry'
+        })
+    ],
+    devtool: "cheap-eval-source-map"
+}
 ]
 
