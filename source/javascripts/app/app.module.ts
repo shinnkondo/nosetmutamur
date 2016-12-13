7929a524796c4b8767f621f4139c2b8fbd93df6e
Shinn Kondo <shinn.kondo@gmail.com>
2016-12-12 23:18:03 -0600

    Use npm for ui-bootstrap. Contents in header have changed to be horizontal in a mobile view since it started to break with vertical align for unknown reasons.

--- source/javascripts/app/app.module.ts
+++ source/javascripts/app/app.module.ts
@@ -1,18 +1,20 @@
-import '../lib/ui-bootstrap-custom-0.13.0'
 import * as angular from 'angular'
 import 'angular-material'
-import 'angular-utils-pagination/dirPagination'
 import 'angular-route'
 import 'angular-aria'
 import 'angular-animate'
 import 'angular-sanitize'
+
+import 'angular-utils-pagination/dirPagination'
+import 'angular-ui-bootstrap/src/collapse'
+
 
 
 import ArticleSearcherService from "./article-searcher/article-searcher.service"
 import CategoryTabsComponent from "./article-searcher/category-tabs/category-tabs.component"
 import ArticleListComponent from "./article-searcher/article-list/article-list.component"
 
-let app = angular.module('NosApp', ['ngMaterial', 'ui.bootstrap', 'ngSanitize', 'ngRoute', 'angularUtils.directives.dirPagination']);
+let app = angular.module('NosApp', ['ngMaterial', 'ui.bootstrap.collapse', 'ngSanitize', 'ngRoute', 'angularUtils.directives.dirPagination']);
 app.config([
     '$mdThemingProvider', function ($mdThemingProvider: angular.material.IThemingProvider) {
         return $mdThemingProvider.theme('default').primaryPalette('blue-grey').accentPalette('deep-purple');
